3045dca0cfe1c205785368c33df9d05d9f4ce47a
Amatullaah Stevenson <160158221@aston.ac.uk>
2025-11-25 23:09:13 +0000

    signup form details

--- UserController.php
+++ UserController.php
@@ -1,9 +1,9 @@
 <?php //Controller: Users 
 
+session_start();
 //include database and user models so can controller can connect to database and use user methods
 include_once 'backend/db_connect.php';//changed to explicity state path
 include_once 'backend/models/User.php';
-session_start(); 
 
 //Handling user-related operations
 class UserController {
@@ -14,9 +14,22 @@ class UserController {
 
 //to check if form submitted for both register and login
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $name = trim($_POST['name'] ?? '');
+
+    $username = trim($_POST['name'] ?? '');
     $email = trim($_POST['email'] ?? '');
     $password = trim($_POST['password'] ?? '');
-    $passwordConfirm = trim($_POST['password_confirm'] ?? '');
+    
+    //register user
+    if (!$username || !$email || !$password) {
+        echo "All fields are required.";
+        exit;
+    }
+    //hashed password for security
+    $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+    //create User model instance
+    $userModel = new User(); //for database connection via user model User.php holding user class - not yet made - create user
+    //default role is customer
+    $role = 'customer';
+    
 }
 ?>
