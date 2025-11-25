9602efc3624126f23242fe43830405ad9b4d6307
Amatullaah Stevenson <160158221@aston.ac.uk>
2025-11-25 21:10:09 +0000

    v1 _ Added base of user controller doc

---
+++ UserController.php
@@ -0,0 +1,14 @@
+<?php //Controller: Users 
+
+//include database and user models so can controller can connect to database and use user methods
+include_once '../config/Database.php';
+include_once '../models/User.php';
+
+//Handling user-related operations
+class UserController {
+    //method to register a new user
+
+    //method to login a user  
+}
+
+?>
