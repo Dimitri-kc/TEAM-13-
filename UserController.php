a4a106e8e597e21d4acdb6c29de42d25533fdbfa
Amatullaah Stevenson <160158221@aston.ac.uk>
2025-11-25 22:49:20 +0000

    check if form submitted for register/login

--- UserController.php
+++ UserController.php
@@ -3,6 +3,7 @@
 //include database and user models so can controller can connect to database and use user methods
 include_once 'backend/db_connect.php';//changed to explicity state path
 include_once 'backend/models/User.php';
+session_start(); 
 
 //Handling user-related operations
 class UserController {
@@ -11,4 +12,11 @@ class UserController {
     //method to login a user  
 }
 
+//to check if form submitted for both register and login
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = trim($_POST['name'] ?? '');
+    $email = trim($_POST['email'] ?? '');
+    $password = trim($_POST['password'] ?? '');
+    $passwordConfirm = trim($_POST['password_confirm'] ?? '');
+}
 ?>
