a8f31a20d87932d2b921075a7f0209a0915b3496
Amatullaah Stevenson <160158221@aston.ac.uk>
2025-11-25 22:25:42 +0000

    added require_role function for admin page access once coded

--- userFunctions.php
+++ userFunctions.php
@@ -9,4 +9,18 @@ function require_login() {
     }
 }
 
+//check if role suits access level, if not deny access and redirect to login
+function require_role($requiredRole) {
+    if(empty($_SESSION['role'])) {
+        header('Location: /login.html');
+        exit; 
+    }
+    //if role doesn't match required then deny access
+    //e.g. if page requires 'admin' role but user is 'user' then deny access
+    if($_SESSION['role'] !== $requiredRole) {
+        header('HTTP/1.1 403 Forbidden');
+        echo "Access denied. You do not have permission to access this page.";
+        exit; 
+    }
+}
 ?>
