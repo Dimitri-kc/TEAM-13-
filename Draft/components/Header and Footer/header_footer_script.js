64baf1085019c6063e0f4e05372fc1535773dea1
Navdeep Malhi <230042537@aston.ac.uk>
2025-11-26 15:04:28 +0000

    Added Header and Footer components

---
+++ Draft/components/Header and Footer/header_footer_script.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const toggleBtn = document.getElementById('menu-toggle-btn');
+    const dropdown = document.getElementById('dropdown-nav');
+    const menuIconImg = document.getElementById('menu-icon-img');
+
+    
+    const menuIconPath = "header_footer_images/icon-menu.png";
+    const closeIconPath = "header_footer_images/icon-close.png";
+
+    toggleBtn.addEventListener('click', (e) => {
+        e.stopPropagation();
+        dropdown.classList.toggle('open');
+
+        
+        if (dropdown.classList.contains('open')) {
+            menuIconImg.src = closeIconPath;
+        } else {
+            menuIconImg.src = menuIconPath;
+        }
+    });
+
+    document.addEventListener('click', (e) => {
+        if (!dropdown.contains(e.target) && dropdown.classList.contains('open')) {
+            dropdown.classList.remove('open');
+            menuIconImg.src = menuIconPath;
+        }
+    });
+});
