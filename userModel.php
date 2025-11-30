f85a3a1a21527210a067810c7395cd47b6b6f53e
Amatullaah Stevenson <160158221@aston.ac.uk>
2025-11-30 16:13:21 +0000

    updated userModel, removed setting session details & password verification as handled in controller

--- userModel.php
+++ userModel.php
@@ -3,31 +3,22 @@ class User {
     private $conn;
 
     public function __construct() {
-        include_once __DIR__ . '/backend/db_connect.php';
+        include_once __DIR__ . '/../config/db_connect.php'; // Adjusted path to include db_connect.php
         $this->conn = $dbConnection;
     }
     
-    //registration method to add new user
+    //registration method to register new user & insert details into database user table
     public function register($name, $surname, $email, $address, $hashedPassword, $role) {
-        $stmt = $this->conn->prepare("INSERT INTO users (username, email, password, role) VALUES (?, ?, ?, ?)"); // Using prepared statements to prevent SQL injection
-        return $stmt->execute();
+        $stmt = $this->conn->prepare("INSERT INTO users (name, email, password, address, role) VALUES (?, ?, ?, ?, ?)"); // Using prepared statements to prevent SQL injection
+        return $stmt->execute([$name, $email, $hashedPassword, $address, $role]); //execute the statement with user details
     }
     
     //login method to authenticate user
     public function login($email, $hashedPassword) {
         $stmt = $this->conn->prepare("SELECT * FROM users WHERE email = ?"); //search for user via email
-        $stmt->execute();
-        $user = $stmt->fetch(PDO::FETCH_ASSOC); //fetch usre data
-        if ($user && password_verify($hashedPassword, $user['password'])) { //checks if password matches hashed password in database
-            // Set session variables
-            $_SESSION['user_ID'] = $user['user_ID']; //
-            $_SESSION['username'] = $user['username'];
-            $_SESSION['role'] = $user['role']; //customer or admin
-            return $user;
-        }
-        return false;
-        echo "Login failed. Invalid email or password.";
+        $stmt->execute([$email]);
+        return $stmt->fetch(PDO::FETCH_ASSOC); //fetch user data
+        //password verification handled in controller
     }
 }
-//check if setting session variables necessary here or in controller as currently in both
 ?>
