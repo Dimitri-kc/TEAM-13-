8c6f5d9542cdf1e482c0482c4ad8d716fe22754f
Amatullaah Stevenson <160158221@aston.ac.uk>
2025-11-30 19:17:53 +0000

    added basket model

---
+++ basketController.php
@@ -0,0 +1,8 @@
+<?php //basketController.php - requests from hmtl pages
+
+//calls basketModel methods to interact with database
+//adds to basket
+//updates quantity 
+//removes items from basket
+//fetches user basket items
+?>
