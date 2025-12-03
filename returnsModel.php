75558d9883b0523691149e203352e9d798836ad2
Dimitri Khair-Cabalan <240124715@aston.ac.uk>
2025-12-03 12:03:58 +0000

    adjustments made to returnsModel

--- returnsModel.php
+++ returnsModel.php
@@ -8,21 +8,21 @@ public function __construct() {
 include_once __DIR__ . '/../db_connect.php';
 $this->conn = $conn;
 }
-// Get ALL returns
+// Get ALL returns.
 public function getAll() {
 $sql = "SELECT * FROM returns";
 $result = $this->conn->query($sql);
 return $result->fetch_all(MYSQLI_ASSOC);
 }
-// Get ONE return
+// Get ONE return.
 public function getById($id) {
 $stmt = $this->conn->prepare("SELECT * FROM returns WHERE return_ID = ?");
 $stmt->bind_param("i", $id);
 $stmt->execute();
 return $stmt->get_result()->fetch_assoc();
 }
-// Create return request
-//Removed productID + returns insert ID
+// Create return request.
+//Removed productID + returns insert ID.
 public function create($orderID, $userID, $reason, $status) {
 $stmt = $this->conn->prepare(
 "INSERT INTO returns (order_ID, user_ID, reason, status)
@@ -36,13 +36,13 @@ return $this->conn->insert_id;
 }
 return false;
 }
-// Update return status
+// Update return status.
 public function update($id, $status) {
 $stmt = $this->conn->prepare("UPDATE returns SET status=? WHERE return_ID=?");
 $stmt->bind_param("si", $status, $id);
 return $stmt->execute();
 }
-// Delete return.
+// Delete return
 public function delete($id) {
 $stmt = $this->conn->prepare("DELETE FROM returns WHERE return_ID = ?");
 $stmt->bind_param("i", $id);
