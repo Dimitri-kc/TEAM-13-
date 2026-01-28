a26f7adefe392b3268261f9ce6df9bf2d5fba539
Manaal Aouttah <240124977@aston.ac.uk>
2026-01-28 11:41:43 +0000

    removed div sections and addes product js file

--- Draft/javascript/livingroom-js/main.js
+++ Draft/javascript/livingroom-js/main.js
@@ -3,3 +3,10 @@ import "./sorting.js";
 import "./price-range.js";
 import "./search.js";
 
+//added for fetch API
+import { loadProducts } from "./products.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const category = document.body.dataset.category;
+    loadProducts(category);
+});
