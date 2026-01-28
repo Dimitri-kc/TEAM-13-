79d2b9e19f512e2cbecaa440518eaadc8e757bd0
Dimitri Khair-Cabalan <240124715@aston.ac.uk>
2026-01-28 12:38:18 +0000

    id names changed

--- Draft/backend/controllers/ProductsController.php
+++ Draft/backend/controllers/ProductsController.php
@@ -6,37 +6,37 @@ class ProductController {
 
 // Get ALL products
 public function index() {
-$model = new ProductModel();
+$model = new ProductsModel();
 return $model->getAll();
 }
 
 // Get ONE product
 public function show($id) {
-$model = new ProductModel();
+$model = new ProductsModel();
 return $model->getById($id);
 }
 
 // Create product
 public function store($name, $desc, $price, $stock, $category, $image) {
-$model = new ProductModel();
+$model = new ProductsModel();
 return $model->create($name, $desc, $price, $stock, $category, $image);
 }
 
 // Update product
 public function update($id, $name, $desc, $price, $stock, $category, $image) {
-$model = new ProductModel();
+$model = new ProductsModel();
 return $model->update($id, $name, $desc, $price, $stock, $category, $image);
 }
 
 // Delete product
 public function destroy($id) {
-$model = new ProductModel();
+$model = new ProductsModel();
 return $model->delete($id);
 }
 //Get product via category name
 public function getByCategory($category) {
     $model = new ProductsModel();
-    return $this->ProductsModel->getProductsByCategory($category);
+    return $model ->getProductsByCategory($category);
 }
 }
 
