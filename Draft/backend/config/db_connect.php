4015a97f2df5ecbafc77991a737733736536bfd6
Manaal Aouttah <240124977@aston.ac.uk>
2026-02-11 10:28:43 +0000

    changed livingroom.html to livingroom.php

--- Draft/backend/config/db_connect.php
+++ Draft/backend/config/db_connect.php
@@ -1,5 +1,5 @@
 <?php
-// $servername = "localhost"; 
+// $servername = "cs2410-web01pvm.aston.ac.uk"; 
 // $username = "cs2team13";
 // $password = "D9Q9c7S6QwcRNAt4Zd4p7JnO2";
 // $dbname = "cs2team13_db";
