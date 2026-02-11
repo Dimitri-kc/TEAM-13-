5f1ce884f75f1a1d75b3caee90df60a37c03f94c
Manaal Aouttah <240124977@aston.ac.uk>
2026-02-11 13:29:26 +0000

    added config for aston db

--- Draft/backend/config/db_connect.php
+++ Draft/backend/config/db_connect.php
@@ -1,13 +1,13 @@
 <?php
-// $servername = "cs2410-web01pvm.aston.ac.uk"; 
-// $username = "cs2team13";
-// $password = "D9Q9c7S6QwcRNAt4Zd4p7JnO2";
-// $dbname = "cs2team13_db";
-
 $servername = "localhost"; 
-$username = "root";
-$password = "";
-$dbname = "team13_local";
+$username = "cs2team13";
+$password = "D9Q9c7S6QwcRNAt4Zd4p7JnO2";
+$dbname = "cs2team13_db";
+
+// $servername = "localhost"; 
+// $username = "root";
+// $password = "";
+// $dbname = "team13_local";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
