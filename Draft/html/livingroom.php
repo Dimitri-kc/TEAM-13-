d173a5844f85f652b8278707ca9fe9610753fd04
Dimitri Khair-Cabalan <240124715@aston.ac.uk>
2026-02-23 13:47:40 +0000

    categories button fixed

--- Draft/html/livingroom.php
+++ Draft/html/livingroom.php
@@ -131,10 +131,11 @@
      data-price="<?php echo $row['price']; ?>" 
      data-rating="<?php echo $row['rating']; ?>"     
      data-keywords="<?php echo $row['keywords']; ?>" 
+     data-category="<?php echo ($row['categories']); ?>"
      data-colour="<?php echo $row['colour']; ?>">
 
-                         <button class="fav-icon-btn" type="button" aria-label="Add to favourites">
-        <img src="../images/header_footer_images/icon-heart.png" alt="">
+    <button class="fav-icon-btn" type="button" aria-label="Add to favourites">
+    <img src="../images/header_footer_images/icon-heart.png" alt="">
     </button>
 
      
