3614f52683f7fe71393ef407f1ff4bcc8da26caa
Amatullaah Stevenson <160158221@aston.ac.uk>
2026-03-21 17:01:03 +0000

    un-commented server connection

--- Draft/backend/config/db_connect.php
+++ Draft/backend/config/db_connect.php
@@ -1,14 +1,14 @@
 <?php
 //error_log("TTHIS IS DB_CONNECT: " . __FILE__);
-// $servername = "localhost"; 
-// $username = "cs2team13";
-// $password = "D9Q9c7S6QwcRNAt4Zd4p7JnO2";
-// $dbname = "cs2team13_db";
+ $servername = "localhost"; 
+ $username = "cs2team13";
+ $password = "D9Q9c7S6QwcRNAt4Zd4p7JnO2";
+ $dbname = "cs2team13_db";
 
-$servername = "localhost"; 
+/* $servername = "localhost"; 
 $username = "root";
 $password = "";
-$dbname = "cs2team13_db";
+$dbname = "cs2team13_db"; */
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -16,5 +16,5 @@ if ($conn->connect_error) {
     throw new Exception("DB connection failed: " . $conn->connect_error);
 }
 
-error_log("Database connection established in db_connect.php: " . __FILE__);
+//error_log("Database connection established in db_connect.php: " . __FILE__);
 ?>
